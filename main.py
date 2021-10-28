fec76bad4a5ede6f3fe3d4a47a37eaa2bbf882dc
ayu <allen.yu@aspiraconnect.com>
2021-10-28 14:42:14 +0800

    py example

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
+'''
 
-print("hello")
+@author: ayu
+'''
+
+def map_list(lst, func):
+    rlst = []
+    for l in lst:
+        rlst.append(func(l)) 
+    return rlst
+
+
+def filter_list(lst, predict):
+    rlst = []
+    for l in lst:
+        if predict(l):
+            rlst.append(l)
+    return rlst
+
+def reduce_lst(lst, default, reduce_func):
+    rst = default;
+    for l in lst:
+        rst = reduce_func(rst,l)
+    return rst;
+
+def reduce_recusive(lst, default, reduce_func):
+    if len(lst) == 0:
+        return default
+    elif len(lst) == 1:
+        return lst[0]
+    elif len(lst) > 1:
+        return reduce_func(lst[0], reduce_recusive(lst[1:] ,default, reduce_func))
+    
+if __name__ == '__main__':
+    lst = [1,3,4,6]
+    print(map_list(lst, lambda x: x + 1))
+    print(filter_list(lst, lambda x: x > 3))  
+    print(reduce_lst(lst, 0,lambda x, y : x + y)) 
+    print(reduce_recusive(lst, 0, lambda x, y : x + y))      
