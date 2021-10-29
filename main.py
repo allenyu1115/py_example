8de4951a7f71444dfb88c460aa7cf86c6595cb3b
ayu <allen.yu@aspiraconnect.com>
2021-10-29 10:53:44 +0800

    group

--- main.py
+++ main.py
@@ -27,14 +27,28 @@ def reduce_lst(lst, default, reduce_func):
     return rst;
 
 
-def reduce_recusive(lst, default, reduce_func):
-    if len(lst) == 0:
-        return default
-    elif len(lst) == 1:
+def reduce_recursive(lst, default, reduce_func):
+    if len(lst) == 1:
         return reduce_func(default, lst[0])
     elif len(lst) > 1:
-        return reduce_func(lst[0], reduce_recusive(lst[1:], default, reduce_func))
+        return reduce_func(lst[0], reduce_recursive(lst[1:], default, reduce_func))
+    else:
+        return default;
 
+
+def get_key(x):
+        for f, key in  group_by_condition.items():
+            if f(x):
+                return key
+            
+def group_by_lst(lst, group_by_key_func):
+    rlst = map_list(lst, lambda x: (group_by_key_func(x), x))
+    rdict = {}
+    for key, i in rlst:
+        value = rdict.get(key, [])
+        value.append(i)
+        rdict[key] = value   
+    return rdict   
 #test!
 if __name__ == '__main__':
     lst = [1, 3, 4, 6]
@@ -43,4 +57,13 @@ if __name__ == '__main__':
     print(map_list(lst, lambda x: x + 1))
     print(filter_list(lst, lambda x: x > 3))  
     print(reduce_lst(lst, 10, lambda x, y: x + y)) 
-    print(reduce_recusive(lst, 10, lambda x, y: x + y))      
+    print(reduce_recursive(lst, 10, lambda x, y: x + y)) 
+    
+    group_by_condition = {lambda x: x < 18 : 'under age', lambda x: x >=18 and x < 30: 'young adult',
+          lambda x: x>=30 and x < 70: 'adult',lambda x: x >=70 : 'senior'}
+    
+    def get_key(x):
+        for cond, key in group_by_condition.items():
+            if cond(x):
+                return key    
+    print(group_by_lst([('a',12),('b',56),('c',34),('c',24),('d',70)], lambda x: get_key(x[1])))
