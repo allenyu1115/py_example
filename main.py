49afb04e79ae914d31920fd46aa8a55a4190b20b
ayu <allenyu>
2021-10-31 22:55:20 +0800

    test

--- main.py
+++ main.py
@@ -48,8 +48,7 @@ def group_by_lst(lst, group_by_key_func):
 
 def test():
     lst = [1, 3, 4, 6]
-    print(lst[0])
-    
+    print(lst[0])   
     print(lst[1:])
     print(map_list(lst, lambda x: x + 1))
     print(filter_list(lst, lambda x: x > 3))  
@@ -63,14 +62,36 @@ def test():
         for cond, key in group_by_condition.items():
             if cond(x):
                 return key
+
     print(group_by_lst([('a', 12), ('b', 56), ('c', 34), ('c', 24), ('d', 70)], lambda x: get_key(x[1])))
 
-        
+
+def check_list(lst,original_position_func):
+    newLst = []
+
+    def inner_check(lst, last_key, original_position , original_position_func):
+        if len(lst) == 0:
+            return newLst
+        else: 
+            ele = lst[0]      
+            if(original_position_func(ele) - original_position) == 1:
+                current_key = last_key
+            else:
+                current_key = last_key + 1
+            newLst.append((current_key, ele))
+            return inner_check(lst[1:], current_key, ele[0], original_position_func)
+    
+    if len(lst) == 0:
+        return newLst
+    
+    return inner_check(lst, -1, original_position_func(lst[0]), original_position_func)
+       
     
 if __name__ == '__main__':
-    testString='ab123b23cdd432a'    
-    x = group_by_lst(map_list(range(0, len(testString)), lambda x : (x, '*' if testString[x].isalpha() else '$'  ,testString[x])), lambda x:x[1])
-    y = {}
-    print(x)
+    testString = 'ab123b23cdd432a'
+    d = {}
+    for key, value in group_by_lst(map_list(range(0, len(testString)), lambda x: (x, '*' if testString[x].isalpha() else '$'  , testString[x])), lambda x:x[1]).items(): 
+        d[key] = [reduce_lst(map_list(i, lambda x: x[1][2]), '', lambda x , y: x + y)  for i in group_by_lst(check_list(value, lambda x: x[0]) , lambda x: x[0]).values() ] 
+    print(d)
     test()
 
