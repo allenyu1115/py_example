1dc0848d9d0dc443c2124afae6e04d69b557a0a1
ayu <allen.yu@aspiraconnect.com>
2021-11-04 15:05:59 +0800

    test

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ generated 10.28
 
 @author: Allen Yu
 '''
+from enum import Enum
+
+
+class CharType(Enum):
+    operator = 1
+    number = 2
+    undefine = 3
+    blank = 4
+    end = 5
 
 
 def map_list(lst, func):
@@ -35,6 +44,7 @@ def reduce_recursive(lst, default, reduce_func):
     else:
         return reduce_func(lst[0], reduce_recursive(lst[1:], default, reduce_func))
 
+
 # tail recursive equals iteration
 def reduce_tail_recursive(lst, default, reduce_func):
     
@@ -61,6 +71,12 @@ def group_by_lst(lst, group_by_key_func):
     return rdict   
 
 
+def get_key(x, group_by_condition):
+        for cond, key in group_by_condition.items():
+            if cond(x):
+                return key
+
+
 def test():
     lst = [1, 3, 4, 6]
     
@@ -74,13 +90,7 @@ def test():
     
     group_by_condition = {lambda x: x < 18: 'under age', lambda x: x >= 18 and x < 30: 'young adult',
           lambda x: x >= 30 and x < 70: 'adult', lambda x: x >= 70: 'senior'}
-    
-    def get_key(x):
-        for cond, key in group_by_condition.items():
-            if cond(x):
-                return key
-
-    print(group_by_lst([('a', 12), ('b', 56), ('c', 34), ('c', 24), ('d', 70)], lambda x: get_key(x[1])))
+    print(group_by_lst([('a', 12), ('b', 56), ('c', 34), ('c', 24), ('d', 70)], lambda x: get_key(x[1], group_by_condition)))
 
 
 # tail recursive equals iteration
@@ -101,8 +111,38 @@ def check_list(lst, category_func):
         return newLst
     
     return inner_check(lst, -1, category_func(lst[0]), category_func)
+
+
+char_type_condition = { lambda char:len(char) == 0: CharType.end,
+                            lambda char: char.isspace(): CharType.blank,
+                            lambda char: char.isnumeric(): CharType.number,
+                            lambda char: char in ['+', '-', '*', '/']: CharType.operator,
+                            lambda char: char: CharType.undefine  }
        
-    
+def get_s_expression(s):
+    def get_s_expression_recursive(s, last_char_type, current_num, last_num, last_operator, combine_f):
+        current_char = '' if len(s) == 0 else s[0]
+        charType = get_key(current_char, char_type_condition)
+        if(charType == CharType.number):
+            return get_s_expression_recursive(s[1:], charType, current_num + current_char, last_num, last_operator, combine_f);
+        elif (charType == CharType.operator):
+            return get_s_expression_recursive(s[1:], charType, '', combine_f(last_operator, last_num, current_num), current_char, combine_f)
+        elif (charType == CharType.blank):
+            return get_s_expression_recursive(s[1:], last_char_type, current_num, last_num, last_operator, combine_f)
+        elif (charType == CharType.end):
+            return combine_f(last_operator, last_num, current_num)
+        else:
+            return ''   
+
+    if len(s) == 0:
+        return s
+    else:
+        return get_s_expression_recursive(s, CharType.undefine, '', '', '',
+                                          lambda operator, left_num, right_num: 
+                                              '(' + operator + ' ' + left_num + ' ' + right_num + ')' if (left_num != '' and operator != '') 
+                                               else right_num)    
+
+
 if __name__ == '__main__':
     testString = 'ab123b23cdd432a'
     x = group_by_lst(check_list(testString, lambda x: x.isalpha()), lambda x:x[0])
@@ -111,4 +151,6 @@ if __name__ == '__main__':
         z = reduce_lst(y, '', lambda x , y: x + y)
         print(z)
     test()
+    xx = get_s_expression('71 + 8 * 76 - 899 / 5')
+    print(xx)
 
