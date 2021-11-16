e9977e382bb7bdf2e2704c6b7020ba54d0b47c0d
ayu <allen.yu@aspiraconnect.com>
2021-11-16 10:40:26 +0800

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 '''
-compare the site_type_for_cc.txt
+compare two files
 
 '''
 from map_reduce_filter import map_list, filter_list
@@ -13,9 +13,9 @@ def read_from_file(file_path):
 
 if __name__ == '__main__':
     
-    site_type_lst = read_from_file(os.path.join('files', 'site_type_for_cc.txt'))
+    site_type_lst = read_from_file(os.path.join('files', 'new_keys.txt'))
     # remove \n 
-    all_icon_lst = map_list(read_from_file(os.path.join('files', 'all_icons_name.txt')), lambda x: x.strip()[:-4]) 
+    all_icon_lst = map_list(read_from_file(os.path.join('files', 'existing_keys.txt')), lambda x: x.strip()[:-4]) 
     # map original list  remove space, remove \r\n, remove \ remove / .  add st_
     arranged_lst = map_list(filter_list(map_list(site_type_lst, lambda x: x.strip().replace(' ', '').replace('\\', '').replace('/', '').lower()),
                       lambda x: True if not x.startswith('-----------------') else False), lambda x: 'st_' + x)
