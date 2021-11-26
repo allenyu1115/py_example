7523f8ee8c9b4d9253c92790407b5a87591c4f43
ayu <ayu@gmail.com>
2021-11-26 23:42:39 +0800

    test

--- logging.py
+++ logging.py
@@ -14,19 +14,16 @@ class version
 '''
 and exists ( select 1 
 from link_attribute as attribute1 
-join attribute1.link as link 
+join attribute1.link as dlink 
 join link_attribute as attribute2 
-on link.id = attribute2.linkid
+on dlink.id = attribute2.linkid
 where attribute1.attributetype = 
-and link.status =
-and link.type_id = 
-and link.id = 
 and attribute1.value = {id}
+and dlink.status =
+and dlink.type_id = 
+and dlink.id = 
 and attribute2.attributetype= 
 and attribute2.value = 
-and link.date >= )
-
-
 '''
 
 class LoggingBase:
