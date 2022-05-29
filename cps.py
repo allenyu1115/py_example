156ab832e3eded374f6334a9bbdfea903f1f0a4d
ayu <ayu@gmail.com>
2022-05-29 22:39:29 +0800

    test

---
+++ cps.py
@@ -0,0 +1,38 @@
+'''
+Created on May 29, 2022
+
+@author: allenyu
+'''
+
+
+def map_lst(lst, f):
+    rls = []
+    def inner_map(lst,f):
+        if len(lst) == 0:
+            return rls
+        else:
+            rls.append(f(lst[0]))
+            return inner_map(lst[1:],f)
+    return inner_map(lst,f)        
+
+
+def cons(r, ls):
+    rls = []
+    rls.append(r)
+    rls.append(ls)
+    return rls
+
+def map_lst_cps(lst,f):
+    def map_inner(lst, accum):
+        if len(lst) == 0:
+            return accum(lst) 
+        else:
+            return map_inner(lst[1:], lambda x: accum(cons(f(lst[0]), x)))
+    return map_inner(lst,lambda x: x)             
+    
+if __name__ == '__main__':
+    lst = map_lst([1,2,3,4], lambda x: x + 1)
+    rlst = map_lst_cps([1,2,3,4], lambda x: x + 1)   
+    print(rlst)
+    print(([1,2,4,6])[0])
+    print(([1,2,4,6])[1:-1])
